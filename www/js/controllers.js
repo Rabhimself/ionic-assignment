97a8562de90ca730f7ba669f1ab897746e97eebc
Rabhimself <rabhimself86@gmail.com>
2015-04-06 17:07:21 +0100

    Today's Meals
    Today's Meals done, to an extent.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,29 +5,65 @@ angular.module('calorific.controllers', [])
   		$scope.foodList = foodList;
 	});
 
-	$scope.showConfirm = function(caloriesAdded) {
+	$scope.showConfirm = function(food) {
    		var confirmPopup = $ionicPopup.confirm({
      		title: 'Add Calories?',
-     		template: 'Are you sure you want to add ' + caloriesAdded + ' ?'
+     		template: 'Add ' + food.calories + ' ?'
    		});
 
    		confirmPopup.then(function(res) {
 	 		if(res) {
-	       		console.log('add ' + caloriesAdded);
-       			calService.addDCals(caloriesAdded);
+       			calService.addDCals(food.calories);
+       			console.log('added ' + food.calories);
+       			if(window.localStorage['todaysMeals'])
+       			{
+	       			window.localStorage['todaysMeals'] += JSON.stringify(food)+ ',';
+	       			console.log('added ' + JSON.stringify(food) + ' to localStorage');       				
+       			}
+       			else{
+       				window.localStorage['todaysMeals'] = JSON.stringify(food)+ ',';
+       			}
 	     	} 
 	     	else {
-	       		console.log('You are not sure');
+	       		console.log('Cancelled');
 	     	}
    		});
 	};
 
 })
+//{"id":"2","name":"Acerola (west indian cherry) raw","weight":"98","measure":"1.0 cup","calories":"31"}
 
+//the dashboard controller
+//this will handle the Daily goal, daily calorie consumption, and the goal
+//A list of food consumed today will also be held
 .controller('DashCtrl', function($scope, calService, $ionicPopup){
 	$scope.calories = calService.getDCals();
 	$scope.goal = calService.getGoal();
 	$scope.burnt = calService.getBurnt();
+	$scope.curDate = new Date;
+	var str = window.localStorage['todaysMeals'];
+	if(str){
+		str = str.substring(0, str.length - 1);
+		str = '[' + str + ']';
+		console.log(' localStor === '+str);
+		try{
+			$scope.todaysList = JSON.parse(str || []);
+		}
+		catch(err)
+		{
+			$scope.todaysList = "";
+		}
+	}
+	else{
+		$scope.todaysList = [];
+	
+	}
+
+	$scope.clearStorage = function(){
+		window.localStorage['todaysMeals'] = "";
+		console.log(' localStor cleared? === '+str);
+		location.reload();
+	};
 
 	$scope.setGoal = function() {
 	$scope.data = {}
@@ -43,7 +79,6 @@ angular.module('calorific.controllers', [])
 		        type: 'button-positive',
 		        onTap: function(e) {
 		          	if (!$scope.data.newGoal) {
-			            //don't allow the user to close unless he enters wifi password
 			            e.preventDefault();
 			        } 
 		          	else {
@@ -88,6 +123,7 @@ angular.module('calorific.controllers', [])
 };
 })
 
+//currently unused, keep for later just in case
 .controller('SingleCtrl', function($scope, $stateParams, foodService){
 	foodService.getFoodList().success(function(foodList){
   		$scope.foodList = foodList;
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,59 @@
 angular.module('calorific.services', [])
 
+//the main service that handles the caloric values
 .factory('calService', function(){
 	
-	var dailyCals = 0;
+	//calories consumed today
+	var dailyCals = { data : 0};
+	//todays goal (limit) for calorie consumption
 	var goal = { data : 0};
+	//tracks how many calories have been burnt via exercise
 	var burnt = {data : 0};
 
 	return{
+		//returns the dailyCals object
 		getDCals: function(){
 			console.log("getDCals called");
 			return dailyCals;
-
 		},
+		//adds calories to the daily tracker
 		addDCals: function(data){
-			dailyCals = dailyCals + parseInt(data);
+			dailyCals.data = dailyCals.data + parseInt(data);
 			console.log("addDCals called");
 		},
+		//set the daily goal, a popup passes in the new goal
 		setGoal: function(data){
 			goal.data = parseInt(data);
 			console.log("setGoal called");
 		},
+		//returns the goal object
 		getGoal: function(){
 			console.log("getGoal called");
 			return goal;
 		},
+		//adds calories to the exercise tracker, a popup passes in the data
 		addBurnt: function(data){
 			burnt.data = burnt.data + parseInt(data);
 			console.log("addBurnt called");
 		},
+		//returns the amount of calories burnt today
 		getBurnt: function(){
 			console.log("getBurnt called");
 			return burnt;
 		}
 	};
 
-})
-    
-.factory('todayService', function(){
+})  
 
-})
-
+//service that returns the USDA database for caloric values for a huge range of foods
+//the database is a JSON file stored locally, the $http depenency will be injected
+//so that the file can be retrieved
 .factory('foodService', function($http){
 	var foodList;
 
 	return{
 			getFoodList: function(){
+				//returns 
 	    		return $http.get('js/foods.json').success(function(response){
 		          		foodList = response;
 		          		return foodList;
